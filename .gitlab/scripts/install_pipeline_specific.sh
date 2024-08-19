5a2d42c5920fb69ebd6bc0e90aa0f633c8354aeb
MertensFlo <fmertens20@gmail.com>
2024-08-19 23:32:16 +0200

    extend reuseablility

--- .gitlab/scripts/install_pipeline_specific.sh
+++ .gitlab/scripts/install_pipeline_specific.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 FLAG_ARM=$1
-ARM_VERSION=$2
+ARM_PATH=$2
+ARM_TAR_PATH=$3
+ARM_DIR_PATH=$4
+ARM_CONDTION=$5
 
 echo "script install pipeline specific"
 if [ $FLAG_ARM ]; then
-    wget -q https://developer.arm.com/-/media/Files/downloads/gnu/$ARM_VERSION/binrel/arm-gnu-toolchain-$ARM_VERSION-x86_64-arm-none-eabi.tar.xz
-    tar -xf arm-gnu-toolchain-$ARM_VERSION-x86_64-arm-none-eabi.tar.xz
-    GITHUB_PATH="$( cd $(git rev-parse --show-cdup); pwd)/arm-gnu-toolchain-$ARM_VERSION-x86_64-arm-none-eabi/bin"
+    wget $ARM_CONDTION $ARM_PATH
+    tar -xf $ARM_TAR_PATH
+    GITHUB_PATH=$ARM_DIR_PATH
 fi
